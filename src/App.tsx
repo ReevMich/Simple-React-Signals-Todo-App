e97868158ad24928398e2918b0b63da5cd9391c9
Michael Reeves <michael@page-vault.com>
2024-02-07 14:11:21 -0800

    convert to typical useState management

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import { Lightbulb, Moon } from "lucide-react"
 
 import { Button } from './components/ui/button';
 import TodoCard from './components/TodoCard';
-import { signal } from '@preact/signals-react';
 import { useTheme } from './components/theme-provider';
+import { useState } from "react";
 
 type Todo = {
   id: string;
@@ -11,24 +11,29 @@ type Todo = {
   completed: boolean;
   created_at: number;
 }
-const todos = signal<Array<Todo>>([]);
+
+// TODO: Replace useState with Signals
+// const todos = signal<Array<Todo>>([]);
 
 function App() {
   const {theme, setTheme} = useTheme();
-
-
+  const [todos, setTodos] = useState<Array<Todo>>([]);
 
   return (
     <>
       <div className='flex flex-col min-h-screen p-4'>
         <div className='flex justify-center'>
-          <Button className='' variant="outline" size="icon" onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
+          <Button
+            variant="outline"
+            size="icon"
+            onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+          >
             {theme === 'dark' && <Lightbulb className="w-4 h-4" />}
             {theme === 'light' && <Moon className="w-4 h-4" />}
           </Button>
         </div>
         <div className='flex justify-center mt-48'>
-          <TodoCard todos={todos} />
+          <TodoCard todos={todos} setTodos={setTodos} />
         </div>
       </div>
     </>
--- src/components/TodoCard.tsx
+++ src/components/TodoCard.tsx
@@ -1,5 +1,4 @@
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "./ui/card";
-import { Signal, useSignal } from "@preact/signals-react";
 
 import { Button } from "./ui/button";
 import { Checkbox } from "./ui/checkbox";
@@ -9,6 +8,7 @@ import { ScrollArea } from "./ui/scroll-area";
 import { Separator } from "./ui/separator";
 import { X } from "lucide-react"
 import { cn } from "@/lib/utils";
+import { useState } from "react";
 
 type Todo = {
     id: string;
@@ -18,38 +18,40 @@ type Todo = {
 }
 
 type TodoCardProps = {
-    todos: Signal<Array<Todo>>;
+    todos: Array<Todo>;
+    setTodos: (todos: Array<Todo>) => void;
 }
 
-function TodoCard({ todos }: TodoCardProps) {
-    const todoText = useSignal('');
+function TodoCard({ todos, setTodos }: TodoCardProps) {
+    const [todoText, setTodoText] = useState<string>("");
+    // const todoText = useSignal('');
 
     const addTodo = (text: string) => {
-        todos.value = [
-            ...todos.value,
+        setTodos([
+            ...todos,
             { id: crypto.randomUUID(), text, completed: false, created_at: Date.now() }
-        ]
+        ])
 
     }
 
     const deleteTodo = (id: string) => {
-        todos.value = todos.value.filter(todo => todo.id !== id)
+        setTodos(todos.filter(todo => todo.id !== id))
     }
 
     const toggleTodo = (id: string) => {
-        todos.value = todos.value.map(todo => {
-            if (todo.id === id) {
-                return { ...todo, completed: !todo.completed }
-            }
-            return todo
-        })
+        setTodos(todos.map((todo) => {
+			if (todo.id === id) {
+				return { ...todo, completed: !todo.completed };
+			}
+			return todo;
+		}));
     }
 
     const addTodoAndClear = () => {
-        if (!todoText.value) return;
+        if (!todoText) return;
 
-        addTodo(todoText.value);
-        todoText.value = '';
+        addTodo(todoText);
+        setTodoText('')
     }
 
     const renderTodoRow = (todo: Todo, index: number) => {
@@ -57,8 +59,12 @@ function TodoCard({ todos }: TodoCardProps) {
             <div key={index} className="flex items-center space-x-2 group">
                 <Checkbox id={todo.id} checked={todo.completed} onCheckedChange={() => toggleTodo(todo.id)} />
                 <Label className={cn("flex-grow", todo.completed && ("line-through text-black/40 dark:text-white/40"))} htmlFor={todo.id}>{todo.text}</Label>
-                <Button className="transition-opacity duration-200 opacity-0 group-hover:opacity-100 hover:bg-black/5 dark:hover:bg-white/5" size="sm" variant="ghost"
-                        onClick={() => deleteTodo(todo.id)}>
+                <Button
+                    className="transition-opacity duration-200 opacity-0 group-hover:opacity-100 hover:bg-black/5 dark:hover:bg-white/5"
+                    size="sm"
+                    variant="ghost"
+                    onClick={() => deleteTodo(todo.id)}
+                >
                     <X className="w-4 h-4 text-black/40 dark:text-white/40" />
                     <span className="sr-only">Delete todo</span>
                 </Button>
@@ -68,27 +74,27 @@ function TodoCard({ todos }: TodoCardProps) {
 
     return (
         <>
-            <Card className='w-[700px]'>
+            <Card className='w-[700px] h-[5000px]'>
                 <CardHeader className="relative bg-white/30 dark:bg-black/30">
                     <CardTitle className="pt-4 pb-6 text-3xl text-center">Todos</CardTitle>
                     <div className='flex space-x-2'>
                         <Input
                             type='text'
                             placeholder='What needs to be done?'
-                            value={todoText.value}
-                            onChange={e => todoText.value = e.currentTarget.value}
+                            value={todoText}
+                            onChange={e => setTodoText(e.currentTarget.value)}
                             onKeyDown={e => e.key === 'Enter' && addTodoAndClear()}
                         />
-                        <Button type="submit" onClick={addTodoAndClear}>Add</Button>
+                        <Button variant="outline" type="submit" onClick={addTodoAndClear}>Add</Button>
                     </div>
                     <Separator className="absolute bottom-0 left-0 right-0 dark:bg-white/10 bg-black/10" />
                 </CardHeader>
                 <CardContent className="mt-4">
-                    {todos.value.length
+                    {todos.length
                         ? (
                             <ScrollArea className="h-96" scrollHideDelay={600} type="auto">
-                                <ul className='space-y-4'>
-                                    {todos.value.map(renderTodoRow)}
+                                <ul className='pr-4 space-y-4'>
+                                    {todos.map(renderTodoRow)}
                                 </ul>
                             </ScrollArea>
                             )
@@ -101,7 +107,7 @@ function TodoCard({ todos }: TodoCardProps) {
                 </CardContent>
                 <CardFooter>
                     <div className="flex justify-center flex-1 font-bold text-black/60 dark:text-white/60">
-                        Tasks Completed: {todos.value.filter(todo => todo.completed).length}
+                        Tasks Completed: {todos.filter(todo => todo.completed).length}
                     </div>
                 </CardFooter>
             </Card>
